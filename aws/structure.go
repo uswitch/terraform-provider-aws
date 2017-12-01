89da65b6640ea1eb238488ed494f8cf6a7452758
Kit Ewbank <Kit_Ewbank@hotmail.com>
2017-12-01 17:41:03 -0500

    Add documentation for aws_vpc_endpoint_service and aws_vpc_endpoint_service_allowed_principal resources.

--- aws/structure.go
+++ aws/structure.go
@@ -3059,3 +3059,13 @@ func flattenMqBrokerInstances(instances []*mq.BrokerInstance) []interface{} {
 
 	return l
 }
+
+func flattenVpcEndpointServiceAllowedPrincipals(allowedPrincipals []*ec2.AllowedPrincipal) []string {
+	result := make([]string, 0, len(allowedPrincipals))
+	for _, allowedPrincipal := range allowedPrincipals {
+		if allowedPrincipal.Principal != nil {
+			result = append(result, *allowedPrincipal.Principal)
+		}
+	}
+	return result
+}
